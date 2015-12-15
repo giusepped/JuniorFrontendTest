da0a19469caad6aee9f2d63419f6af749dd6e7ff
giuseppe de santis <giusdesan@gmail.com>
2015-12-15 11:19:02 +0000

    added all variables to index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var searchResultContainer = document.getElementsByClassName("searchUserResult")[0],
+    errorMessageContainer = document.getElementsByClassName("errorMessage")[0],
+    searchButton = document.getElementById("searchButton"),
+    userSearched = document.getElementById("userSearched"),
+    avatar = document.getElementById("avatar"),
+    username = document.getElementById("username"),
+    fullname = document.getElementById("fullname"),
+    userBio = document.getElementById("bio"),
+    tableReposBody = document.getElementsByTagName("tbody")[0];
