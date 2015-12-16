2f33c1acd5189e0450b8dcc18e2a831ea87ea327
giuseppe de santis <giusdesan@gmail.com>
2015-12-16 15:36:43 +0000

    SearchSpec first configuration

---
+++ spec/SearchSpec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Search', function() {
+
+  var search;
+
+  beforeEach(function() {
+    search = new Search();
+  });
+
+  // it('first test', function() {
+  //   expect(serach.something).toEqual("");
+  // });
+});
