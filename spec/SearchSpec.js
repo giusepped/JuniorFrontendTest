3a7b971351e9132a8e42967ebc2190a7e6184862
giuseppe de santis <giusdesan@gmail.com>
2015-12-16 17:13:45 +0000

    passing all tests for setting user data

--- spec/SearchSpec.js
+++ spec/SearchSpec.js
@@ -10,27 +10,44 @@ describe('Search', function() {
 
   describe('At the beginning', function() {
     it('should start with an empty username', function() {
-      expect(search.username).toEqual("");
+      expect(search.username).toEqual('');
     });
 
     it('should start with an empty fullname', function() {
-      expect(search.fullname).toEqual("");
+      expect(search.fullname).toEqual('');
     });
 
     it('should start with an empty avatar', function() {
-      expect(search.avatar).toEqual("");
+      expect(search.avatar).toEqual('');
     });
 
     it('should start with an empty bio', function() {
-      expect(search.bio).toEqual("");
+      expect(search.bio).toEqual('');
     });
   });
 
   describe('After searching', function() {
     it('should be able to set the username', function() {
-      search.setUsername("giusepped");
-      expect(search.username).toEqual("giusepped");
+      search.setUsername('giusepped');
+      expect(search.username).toEqual('giusepped');
     });
+
+    it('should be able to set the full name', function() {
+      search.setFullname('Giuseppe De Santis');
+      expect(search.fullname).toEqual('Giuseppe De Santis');
+    });
+
+    it('should be able to set the avatar', function() {
+      search.setAvatar("https://avatars.githubusercontent.com/u/3399076?v=3");
+      expect(search.avatar).toEqual("https://avatars.githubusercontent.com/u/3399076?v=3");
+    });
+
+    it('should be able to set the bio', function() {
+      search.setUsername('giusepped');
+      search.setBio(null);
+      expect(search.bio).toEqual('giusepped has written no bio');
+    });
+
   });
 
 
--- src/Search.js
+++ src/Search.js
@@ -10,3 +10,15 @@ function Search() {
 Search.prototype.setUsername = function(username) {
   this.username = username;
 }
+
+Search.prototype.setFullname = function(fullname) {
+  this.fullname = fullname;
+}
+
+Search.prototype.setAvatar = function(avatar) {
+  this.avatar = avatar;
+}
+
+Search.prototype.setBio = function(bio) {
+  this.bio = (bio === null) ? this.username + " has written no bio" : bio;
+}
